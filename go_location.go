fc7ed3bc59a1c406ee6c36f51fe1bbe8a8f8900a
Abdulazeez Abdulazeez Adeshina <laisibizness@gmail.com>
2019-10-10 18:14:51 +0100

    Added more methods to the package and also renamed package - linter refused underscore in package name

--- go_location.go
+++ go_location.go
@@ -1,18 +1,92 @@
-package go_location
+package golocation
 
 import (
-	"fmt"
-	"io/ioutil"
-	"os"
+	"encoding/json"
 )
 
-func AllCountries() []byte {
-	countries, err := os.Open("./db/countries.json")
+func AllCountries() Country {
+	var countries Country
 
-	if err != nil {
-		fmt.Println(err)
+	_ = json.Unmarshal(loadCountries(), &countries)
+	return countries
+
+}
+
+func GetCountry(id int) Country {
+	var country Country
+
+	_ = json.Unmarshal(loadCountries(), &country)
+
+	for idx, cty := range country {
+		if id == country[idx].ID {
+			return Country{cty}
+		}
 	}
+	return nil
+}
 
-	countriesJson, _ := ioutil.ReadAll(countries)
-	return countriesJson
+func AllStates() State {
+	var states State
+
+	_ = json.Unmarshal(loadStates(), &states)
+	return states
+}
+
+func GetState(id int) interface{} {
+	var state State
+
+	_ = json.Unmarshal(loadStates(), &state)
+
+	for idx, s := range state {
+		if id == state[idx].ID {
+			return s
+		}
+	}
+	return nil
+}
+
+// TODO: Return all matches.
+func GetStatesByCtyID(ctyid int) State {
+	var state State
+
+	_ = json.Unmarshal(loadStates(), &state)
+	for i, s := range state {
+		if ctyid == state[i].CountryID {
+			return State{s}
+		}
+	}
+	return nil
+}
+
+func AllCities() City {
+	var cities City
+
+	_ = json.Unmarshal(loadCountries(), &cities)
+	return cities
+}
+
+func GetCity(id int) interface{} {
+	var city City
+
+	_ = json.Unmarshal(loadCities(), &city)
+
+	for idx, c := range city {
+		if id == city[idx].ID {
+			return c
+		}
+	}
+	return nil
+}
+
+func GetCities(stateid int) interface{} {
+	var city City
+
+	_ = json.Unmarshal(loadCities(), &city)
+
+	for idx, cities := range city {
+		if stateid == city[idx].StateID {
+			return cities
+		}
+	}
+	return nil
 }
