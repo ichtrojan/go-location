4903b0dc891fc5f22aa60b82bf06aa989a82f538
ichtrojan <ordrizzy@gmail.com>
2019-11-12 00:48:30 +0100

    Rewrote the Models to fit

--- models.go
+++ models.go
@@ -1,20 +1,20 @@
 package golocation
 
-type Country []struct {
-	ID        int    `json:"id"`
+type Country struct {
+	Id        int    `json:"id"`
 	Code      string `json:"code"`
 	Name      string `json:"name"`
 	Phonecode int    `json:"phonecode"`
 }
 
-type State []struct {
-	ID        int    `json:"id"`
+type State struct {
+	Id        int    `json:"id"`
 	Name      string `json:"name"`
-	CountryID int    `json:"country_id"`
+	CountryId int    `json:"country_id"`
 }
 
-type City []struct {
-	ID      int    `json:"id"`
+type City struct {
+	Id      int    `json:"id"`
 	Name    string `json:"name"`
-	StateID int    `json:"state_id"`
+	StateId int    `json:"state_id"`
 }
